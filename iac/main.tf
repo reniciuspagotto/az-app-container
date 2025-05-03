5e35c0184abcdf3c09788ed0f5b9a16dc98929e8
Renicius Pagotto <renicius.pagotto@outlook.com>
2025-05-02 21:18:56 -0300

    New ACR task update

--- iac/main.tf
+++ iac/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_container_registry_task" "mytask" {
     dockerfile_path      = "Dockerfile"
     context_path         = "https://github.com/reniciuspagotto/az-netapp-container#main:."
     context_access_token = var.github_pat
-    image_names          = ["azapp:latest"]
+    image_names          = ["azapp:{{.Run.Commit}}"]
   }
 
   source_trigger {
